5dc2deaa0c6cbf70cc09136336d8cd58c53e6fae
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-22 00:33:28 +0300

    backend: upload ads api

---
+++ ecommerce-server/upload-ads.php
@@ -0,0 +1,42 @@
+<?php
+// Connection
+include("connection.php");
+//required file
+require __DIR__ . '/../vendor/autoload.php';
+
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+// Convert data
+$jwtFunction = require('auth.php');
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+$headers = getallheaders();
+
+$jwtInfo = $jwtFunction(json_encode(['jwt' => explode(" ", $headers["Authorization"])[1]]));
+
+$json = json_decode($jwtInfo, true); // decode the JSON into an associative array
+
+if ($json['user']['user_type'] == "Seller") {
+    if (isset($json['user']['id']) && isset($_POST['picture_url'])) {
+
+        extract($_POST);
+        $query = $mysqli->prepare("insert into ads (picture_url, seller_id)  value(?,?)");
+        $query->bind_param("ss", $picture_url ,$json['user']['id'] );
+        $query->execute();
+        $result = $query->get_result();
+        $response = [];
+
+        if (($query->error) == "") {
+            $response["success"] = true;
+            $response["jwt"] = $json["JWT"];
+            echo json_encode($response);
+        } else {
+            $response["success"] = false;
+            $response["error"] = "Wrong Credentials";
+            echo json_encode($response);
+        }
+    }
+}
+?>
