249239df8f021332ea4b1b60d6adc13574718d1c
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-21 16:15:07 +0300

    add register

--- ecommerce-server/register.php
+++ ecommerce-server/register.php
@@ -10,5 +10,33 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorizatio
 // Convert data
 $jwtFunction = require('auth.php');
 $_POST = json_decode(file_get_contents('php://input'), true);
+if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['f_name']) && isset($_POST['l_name']))
+{
+    extract($_POST);
+    $password=hash('sha256',$_POST['password']);
 
+    $query = $mysqli->prepare("INSERT INTO `users` (`f_name`, `l_name`, `email`, `password`) VALUES (?, ?, ?, ?);");
+    $query->bind_param("ssss", $f_name ,$l_name , $email, $password);
+    $query->execute(); 
+
+    if(isset($query->insert_id)){
+        $response["success"] =true;
+        $info=[];
+    
+        $jwt = $jwtFunction(json_encode([
+            "id"=> $query->insert_id,
+            "name"=> $f_name." ".$l_name,
+            "email"=> $email,
+            "user_type"=> 'Client'
+        ]));
+        $jwt = json_decode($jwt) ;  
+        $response["jwt"] = $jwt->JWT;
+        echo json_encode($response);
+    }
+    else{
+        $response["success"] =false;
+        $response["error"] ="Wrong Credentials";
+        echo json_encode($response);
+    }
+}
 ?>
