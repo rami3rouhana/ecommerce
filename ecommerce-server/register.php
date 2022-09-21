49c8af5e59b736d863cdec9918146d37dd5487d0
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-21 15:04:50 +0300

    add register api

---
+++ ecommerce-server/register.php
@@ -0,0 +1,14 @@
+<?php
+// Connection
+include("connection.php");
+//required file
+require __DIR__ . '/../vendor/autoload.php';
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+// Convert data
+$jwtFunction = require('auth.php');
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+?>
