8a6877a7d1e816a7ac44cc894045f8976ce54edf
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-22 13:56:35 +0300

    add use effect

--- seller-frontend/scripts/index.js
+++ seller-frontend/scripts/index.js
@@ -1,5 +1,7 @@
 import { useState } from "./useState.js";
+import { useEffect } from "./useEffect.js";
 
+useEffect();
 let [category, setCategory] = useState("");
 let [product, setProduct] = useState([]);
 let [discount, setDiscount] = useState(0);
@@ -8,43 +10,7 @@ let [date, setDate] = useState([]);
 let [adName, setAdName] = useState([]);
 let [adUrl, setAdUrl] = useState([]);
 let [addescription, setAdDescription] = useState([]);
-const years = [2020, 2021, 2022];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-const days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-
-window.onload = () => {
-    if (document.getElementById("add-product-categories")) {
-        const categories = ['category', 'potato', 'batata'];
-        let options = "";
-        categories.forEach(category => {
-            options += `<option value="${category}">${category}</option>`
-        })
-        document.getElementById("add-product-categories").innerHTML = options;
-    }
-    if (document.getElementById("year")) {
-        const categories = ['category', 'potato', 'batata'];
-        let options = "";
-        years.forEach(year => {
-            options += `<option value="${year}">${year}</option>`
-        })
-        document.getElementById("year").innerHTML = options;
-    }
-    if (document.getElementById("month")) {
-        let options = "";
-        months.forEach((month, i) => {
-            options += `<option value="${i}">${month}</option>`
-        })
-        document.getElementById("month").innerHTML = options;
-    }
-    if (document.getElementById("day")) {
-        let options = "";
-        days.forEach(day => {
-            options += `<option value="${day}">${day}</option>`
-        })
-        document.getElementById("day").innerHTML = options;
-    }
-}
 
 if (document.getElementById("add-category")) {
     document.getElementById("add-category").addEventListener("click", () => {
