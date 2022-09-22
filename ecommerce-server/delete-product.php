b53b54ab111bb3c3fdebd238e86c9b6fd39dcdc4
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-22 12:55:25 +0300

    backend: fix delete product

--- ecommerce-server/delete-product.php
+++ ecommerce-server/delete-product.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 // Connection
 include("connection.php");
@@ -39,48 +39,3 @@ if ($json['user']['user_type'] == "Seller") {
         }
     }
 }
-||||||| c67b669
-=======
-<?php
-// Connection
-include("connection.php");
-//required file
-require __DIR__ . '/../vendor/autoload.php';
-
-//headers
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-// Convert data
-$jwtFunction = require('auth.php');
-$_POST = json_decode(file_get_contents('php://input'), true);
-
-$headers = getallheaders();
-
-$jwtInfo = $jwtFunction(json_encode(['jwt' => explode(" ", $headers["Authorization"])[1]]));
-
-$json = json_decode($jwtInfo, true); // decode the JSON into an associative array
-
-if ($json['user']['user_type'] == "Seller") {
-    if (isset($_POST['id'])) {
-        
-        extract($_POST);
-
-        $query = $mysqli->prepare("DELETE FROM Products WHERE id=? ");
-        $query->bind_param("i", $id );
-        $query->execute();
-        $result = $query->get_result();
-        $response = [];
-        if (($query->error) == "") {
-            $response["success"] = true;
-            $response["jwt"] = $json["JWT"];
-            echo json_encode($response);
-        } else {
-            $response["success"] = false;
-            $response["error"] = "Wrong Credentials";
-            echo json_encode($response);
-        }
-    }
-}
-?>
->>>>>>> a54a14d522cd22ba70100de3db762580b2eadb14
