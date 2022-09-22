a79b8e1fedfb32f579a6f2937dec2640ed90a1c3
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-23 00:46:50 +0300

    frontend: refactor login

--- client-frontend/scripts/login.js
+++ client-frontend/scripts/login.js
@@ -1,16 +1,18 @@
 import { useState } from "../../admin-electron/scripts/useState.js";
 
-let [email,setEmail] = useState("");
-let [password,setPassword] = useState("");
-export const login = async () =>{
-    const url="http://localhost/ecommerce/ecommerce-server/login.php";
-    setEmail(document.getElementById("login-username").value);
-    setPassword(document.getElementById("login-password").value);
-    const data = JSON.stringify({
-        email:email(),
-        password:password()
-    })
-    const user = await axios.post(url,data);
-    localStorage.setItem("token",user.data.jwt)
-
-}
+let [email, setEmail] = useState("");
+let [password, setPassword] = useState("");
+export const login = () => {
+    if (document.getElementById("login-btn")) {
+        document.getElementById("login-btn").addEventListener("click", async () => {
+            const url = "http://localhost/ecommerce/ecommerce-server/login.php";
+            setEmail(document.getElementById("login-username").value);
+            setPassword(document.getElementById("login-password").value);
+            const data = JSON.stringify({
+                email: email(),
+                password: password()
+            })
+            const user = await axios.post(url, data);
+            localStorage.setItem("token", user.data.jwt)
+        })
+}}
