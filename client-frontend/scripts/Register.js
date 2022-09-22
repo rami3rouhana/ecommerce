531d4482976273fb92d17dc8850045c211b50b8f
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-22 23:25:39 +0300

    frontend: add register js

---
+++ client-frontend/scripts/Register.js
@@ -0,0 +1,23 @@
+
+export const Register = () => {
+    document.getElementById("register-btn").addEventListener('click', async () => {
+        const url = "http://localhost/ecommerce/ecommerce-server/register.php";
+        const f_name = document.getElementById('username-register').value;
+        const email = document.getElementById('email-register').value;
+        const password = document.getElementById('password-register').value;
+        const confirm_password = document.getElementById('confirm-register').value;
+        if (password == confirm_password) {
+            const data = JSON.stringify({
+                f_name,
+                email,
+                password
+            });
+            const user = await axios.post(url, data);
+            localStorage.setItem('token', user.data.jwt);
+        }else {
+            alert('Wrong password')
+        }
+    })
+
+
+}
--- client-frontend/scripts/index.js
+++ client-frontend/scripts/index.js
@@ -1,3 +1,4 @@
 import { GetProducts } from "./GetProducts.js";
-
+import { Register } from "./Register.js";
 GetProducts();
+Register();
