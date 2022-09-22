9c3bfb10e9915fe7a5c0866e435ee07da6a3313d
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-22 23:24:58 +0300

    frontend: fix get products

--- client-frontend/scripts/GetProducts.js
+++ client-frontend/scripts/GetProducts.js
@@ -1,11 +1,14 @@
 import { Product } from "./Product.js";
 
-export const GetProducts = async () =>{
-    let productsHTML = ""
-    const url="http://localhost/ecommerce/ecommerce-server/receive-all-products.php";
-    const products = await axios.get(url);
-    products.data.map(product=>{
-        productsHTML += Product(product);
-    })
-    document.getElementById("products-row").innerHTML = productsHTML;
+export const GetProducts = async () => {
+    if (document.getElementById("products-row")) {
+        let productsHTML = ""
+        const url = "http://localhost/ecommerce/ecommerce-server/receive-all-products.php";
+        const products = await axios.get(url);
+        products.data.map(product => {
+            productsHTML += Product(product);
+        })
+        document.getElementById("products-row").innerHTML = productsHTML;
+    }
+
 }
