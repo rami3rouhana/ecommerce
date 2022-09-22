c0749ee6d2e9df4365218c36a3198e123619b03c
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-22 16:50:26 +0300

    frontend: add seller

--- admin-electron/scripts/AddSeller.js
+++ admin-electron/scripts/AddSeller.js
@@ -1,29 +1,34 @@
-import { useState } from "./useState";
+import { useState } from "./useState.js";
 
-export const AddSeller = () =>{
+export const AddSeller = () => {
+    let [email, setEmail] = useState("");
+    let [name, setName] = useState("");
+    let [password, setPassword] = useState("");
 
     if (document.getElementById("add-seller")) {
         document.getElementById("add-seller").addEventListener("click", () => {
-            debugger
+            // popup open
             document.getElementById("seller-popup").classList.remove('hidden')
             document.getElementById("cover").classList.remove('hidden')
             // Get user Value
-            setProduct(document.getElementById("email").value);
-            setProduct([product(), document.getElementById("add-product-price").value]);
-            setProduct([product(), document.getElementById("add-product-categories").value]);
-    
-            console.log(product());
-    
-            // add category api
-    
-            // Insert Value into table
-            const categoryName = document.createElement('li');
-            categoryName.classList.add('td');
-            categoryName.classList.add('color-purple');
-            categoryName.innerText = product()[0][0];
-            document.getElementById("product").appendChild(categoryName);
-            document.getElementById("add-product-name").value = "";
-            document.getElementById("add-product-price").value = "";
+            document.getElementById("add-btn").addEventListener("click", () => {
+                setEmail(document.getElementById("add-email").value);
+                setName(document.getElementById("add-name").value);
+                setPassword(document.getElementById("add-password").value);
+                // add category api
+                console.log(name() + password());
+                // Insert Value into table
+                document.getElementById("add-email").value = "";
+                document.getElementById("add-name").value = "";
+                document.getElementById("add-password").value = "";
+                document.getElementById("seller-popup").classList.add('hidden')
+                document.getElementById("cover").classList.add('hidden')
+            })
+            document.getElementById("close-btn-add").addEventListener("click", () => {
+                document.getElementById("seller-popup").classList.add('hidden')
+                document.getElementById("cover").classList.add('hidden')
+            })
+
         })
     }
 }
