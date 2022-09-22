14c8cf6b28ba318b54045b9c849418c5392d2d6f
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-22 15:59:44 +0300

    frontend: add client

--- admin-electron/scripts/index.js
+++ admin-electron/scripts/index.js
@@ -1,7 +1,10 @@
 import { useState } from "./useState.js";
 import { PageLocations } from './PageLocations.js';
 import { GetStats } from './GetStats.js';
+import { Clients } from './Clients.js';
+
 
 
 PageLocations();
 GetStats();
+Clients();
