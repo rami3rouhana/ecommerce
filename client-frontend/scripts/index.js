b7f180c9295464a3e1fbb72eb51c314ff41fde75
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-23 00:43:08 +0300

    frontend: add login

--- client-frontend/scripts/index.js
+++ client-frontend/scripts/index.js
@@ -1,3 +1,13 @@
 import { GetProducts } from "./GetProducts.js";
+import { login } from "./login.js";
 
 GetProducts();
+
+
+if (document.getElementById("login-btn")) {
+    document.getElementById("login-btn").addEventListener("click", () => {
+        login();
+    })
+
+
+}
