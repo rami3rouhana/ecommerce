af423a9df26f127812a5798d18bb91f2bd86c258
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-22 14:29:31 +0300

    frontend: add pagelocation function

---
+++ seller-frontend/scripts/PageLocations.js
@@ -0,0 +1,38 @@
+export const PageLocations = () => {
+    if (document.getElementById("products"))
+        document.getElementById("products").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/products.html";
+        })
+    if (document.getElementById("products-nav"))
+        document.getElementById("products-nav").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/products.html";
+        })
+    if (document.getElementById("categories"))
+        document.getElementById("categories").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/categories.html";
+        })
+    if (document.getElementById("ads"))
+        document.getElementById("ads").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/ads.html";
+        })
+    if (document.getElementById("statistics"))
+        document.getElementById("statistics").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/statistics.html";
+        })
+    if (document.getElementById("vouchers"))
+        document.getElementById("vouchers").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/vouchers.html";
+        })
+    if (document.getElementById("discounts"))
+        document.getElementById("discounts").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/discounts.html";
+        })
+    if (document.getElementById("discounts-nav"))
+        document.getElementById("discounts-nav").addEventListener("click", () => {
+            window.location.href = "../seller-frontend/discounts.html";
+        })
+    document.getElementById("logout").addEventListener("click", () => {
+        localStorage.removeItem("token");
+        window.location.href = "../client-frontend/index.html";
+    })
+}
