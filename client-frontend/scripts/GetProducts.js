65f5317e1ddb6462e34b9371fb7af920e113c5f1
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-22 22:32:00 +0300

    frontend: get products axios

--- client-frontend/scripts/GetProducts.js
+++ client-frontend/scripts/GetProducts.js
@@ -1,5 +1,11 @@
 import { Product } from "./Product.js";
 
-export const GetProducts = () =>{
-    
+export const GetProducts = async () =>{
+    let productsHTML = ""
+    const url="http://localhost/ecommerce/ecommerce-server/receive-all-products.php";
+    const products = await axios.get(url);
+    products.data.map(product=>{
+        productsHTML += Product(product);
+    })
+    document.getElementById("products-row").innerHTML = productsHTML;
 }
