7961fdb3ca7e29a34199bbe046021ef4b5a2090c
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-23 00:10:08 +0300

    frontend: add favorite axios

--- client-frontend/scripts/index.js
+++ client-frontend/scripts/index.js
@@ -1,4 +1,6 @@
 import { GetProducts } from "./GetProducts.js";
 import { Register } from "./Register.js";
+import { GetFavorites } from "./GetFavorites.js";
 GetProducts();
 Register();
+GetFavorites();
