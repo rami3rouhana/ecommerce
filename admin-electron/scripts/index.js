42c8ec620a5c5b2b18678dc0bebb3d371f3079f7
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-22 16:49:04 +0300

    frontend: refacter index naming

--- admin-electron/scripts/index.js
+++ admin-electron/scripts/index.js
@@ -3,10 +3,10 @@ import { useState } from "./useState.js";
 import { PageLocations } from './PageLocations.js';
 import { GetStats } from './GetStats.js';
 import { Clients } from './Clients.js';
-import { addSeller } from "./add-seller.js";
+import { AddSeller } from "./AddSeller.js";
 
 
 PageLocations();
 GetStats();
 Clients();
-addSeller();
+AddSeller();
