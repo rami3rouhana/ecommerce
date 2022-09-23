fc8db4ebc2aafc6965d06147bfdd1cd32e577215
Mohamad Nassar <mnassar57@gmail.com>
2022-09-23 23:48:45 +0300

    frontend: bar graphs for top sellers implemented

--- admin-electron/scripts/Axios/ReceiveStats.js
+++ admin-electron/scripts/Axios/ReceiveStats.js
@@ -1,14 +1,43 @@
+let xValues = []
+let yValues = []
+let barColors = ["red", "green","blue","orange","brown"];
+const chartsDiv = document.getElementById("main-display=charts");
+
 export const ReceiveStats = async (data) => {
+
     if(data.user_type == 'Client'){
 
     }
     else if(data.user_type == 'Seller'){
         console.log(data);
+        xValues = []
+        yValues = []
             const url = "http://localhost/ecommerce/ecommerce-server/select-revenue.php";
             const response = await axios.post(url, data, {headers: {'Authorization': `token ${localStorage.getItem("token")}` 
                  // 
-        }});console.log(await response.data['topgroups']);
+        }});
+
+        response.data['topgroups'].map(top => {
+            xValues.push(top.f_name);
+            yValues.push(top.totalrev);
+        })
+        chartsDiv.innerHTML = '<canvas id="myChart" style="width:100%;"></canvas>';
+        new Chart("myChart", {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+            }]
+        },
+        options: {
+            legend: {display: false},
+            title: {
+            display: true,
+            text: "Top 5 Sellers"
+            }
+        }
+        });
     }
-
-
 }
