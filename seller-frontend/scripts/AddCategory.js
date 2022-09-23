c2a1c07a2deda37f8ad605cbdd48eebddcfcb0cd
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-24 00:32:41 +0300

    frontend: add seller button

--- seller-frontend/scripts/AddCategory.js
+++ seller-frontend/scripts/AddCategory.js
@@ -17,6 +17,19 @@ export const AddCategory = (category,setCategory) =>{
             categoryName.innerText = category();
             document.getElementById("name").appendChild(categoryName);
             document.getElementById("add-category-name").value = '';
+            const actions = document.createElement('li');
+            actions.classList.add('td');
+
+            const updateBtn = document.createElement('img');
+            updateBtn.classList.add("clickable-edit");
+            updateBtn.src="./images/edit-button.svg";
+            const deleteBtn = document.createElement('img');
+            deleteBtn.classList.add("clickable-delete");
+            deleteBtn.src="./images/delete-button.svg";
+            actions.appendChild(updateBtn);
+            actions.appendChild(deleteBtn);
+            document.getElementById("actions").appendChild(actions)
+            debugger
         })
     }
 }
