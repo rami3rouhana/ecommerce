0261ac18fa948b9e119d2cabb7c6cf51d9d6645e
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-23 21:18:58 +0300

    frontend: merge get products

--- client-frontend/scripts/GetProducts.js
+++ client-frontend/scripts/GetProducts.js
@@ -1,15 +1,6 @@
 import { Product } from "./Product.js";
 
 export const GetProducts = async () => {
-<<<<<<< HEAD
-    let productsHTML = ""
-    const url = "http://localhost/ecommerce/ecommerce-server/receive-all-products.php";
-    const products = await axios.get(url);
-    products.data.map(product => {
-        productsHTML += Product(product);
-    })
-    document.getElementById("products-row").innerHTML = productsHTML;
-=======
     if (document.getElementById("products-row")) {
         let productsHTML = ""
         const url = "http://localhost/ecommerce/ecommerce-server/receive-all-products.php";
@@ -19,6 +10,4 @@ export const GetProducts = async () => {
         })
         document.getElementById("products-row").innerHTML = productsHTML;
     }
-
->>>>>>> 0fde945eddeff7c2e9e1274d025678f2901e257e
 }
