2234aad3c261cc19d409a0eb307ef50362096b72
Mohamad Nassar <mnassar57@gmail.com>
2022-09-24 23:38:29 +0300

    backend: receive voucher implemented

---
+++ client-frontend/scripts/Vouchers.js
@@ -0,0 +1,12 @@
+export const Vouchers = () => {
+    
+
+
+
+    return (`  
+        <select>
+            <option value="0">Select car:</option>
+            <option value="1">Audi</option>
+        </select>
+    `)
+}
