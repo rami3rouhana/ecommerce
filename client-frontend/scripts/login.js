c22ab0fbd1f0dfdff28c8ad7abf577da5b8eb895
Mohamad Nassar <mnassar57@gmail.com>
2022-09-24 17:23:46 +0300

    frontend: fixing login validation

--- client-frontend/scripts/login.js
+++ client-frontend/scripts/login.js
@@ -2,9 +2,10 @@ import { useState } from "../../admin-electron/scripts/useState.js";
 
 let [email, setEmail] = useState("");
 let [password, setPassword] = useState("");
-export const login = () => {
+export const login = async () => {
     if (document.getElementById("login-btn")) {
         document.getElementById("login-btn").addEventListener("click", async () => {
+            console.log("gg");
             const url = "http://localhost/ecommerce/ecommerce-server/login.php";
             setEmail(document.getElementById("login-username").value);
             setPassword(document.getElementById("login-password").value);
@@ -12,7 +13,13 @@ export const login = () => {
                 email: email(),
                 password: password()
             })
+            console.log(data);
             const user = await axios.post(url, data);
-            localStorage.setItem("token", user.data.jwt)
+            console.log(user);
+            if(user.data.jwt != undefined){
+                localStorage.setItem("token", user.data.jwt)
+                window.location.href = 'http://localhost/ecommerce/client-frontend/products.html';
+            }
+            
         })
 }}
