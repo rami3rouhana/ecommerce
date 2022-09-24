09d16863449be4bafc5fc96ec172b889386744dc
Mohamad Nassar <mnassar57@gmail.com>
2022-09-24 23:09:14 +0300

    frontend: added discounted items div

--- client-frontend/scripts/ApplyDiscount.js
+++ client-frontend/scripts/ApplyDiscount.js
@@ -1,5 +1,9 @@
-export const ApplyDiscount = () => {
-    document.getElementById("apply-discount").addEventListener("click", () => {
+export const ApplyDiscount = async ()  => {
+    document.getElementById("apply-discount").addEventListener("click", async ()  => {
         console.log(document.getElementById("discount-code").value);
+        let discount_code = (document.getElementById("discount-code").value);
+        const url = "http://localhost/ecommerce/ecommerce-server/apply-discount.php";
+        const response = await axios.post(url, {discount_code: discount_code}, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}});
+        console.log(response);
     })
 }
