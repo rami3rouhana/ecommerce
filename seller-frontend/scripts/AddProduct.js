c8be6facedcfb9f3c006daf26fee23a5c0119d44
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-24 14:57:45 +0300

    frontend: add products table

--- seller-frontend/scripts/AddProduct.js
+++ seller-frontend/scripts/AddProduct.js
@@ -9,18 +9,34 @@ export const AddProduct = (e) =>{
             setProduct(document.getElementById("add-product-name").value);
             setProduct([product(), document.getElementById("add-product-price").value]);
             setProduct([product(), document.getElementById("add-product-categories").value]);
-    
-            console.log(product());
+
     
             // add category api
     
             // Insert Value into table
-            const categoryName = document.createElement('tr');
-            categoryName.classList.add('td');
-            categoryName.classList.add('color-purple');
-            categoryName.innerText = product()[0][0];
-            document.getElementById("product").appendChild(categoryName);
-            e.document.getElementById("actions").appendChild('<i class="fa fa-trash-o" aria-hidden="true"></i>');
+            const category = document.createElement('tr');
+            category.classList.add('td');
+            category.classList.add('color-purple');
+            category.classList.add('border');
+            const categoryName = document.createElement('td');
+            const categoryActions = document.createElement('td');
+
+            const delete_img = document.createElement('img');
+            delete_img.classList.add('icon')
+            delete_img.src = "./images/delete-button.svg"
+
+            const edit_img = document.createElement('img');
+            edit_img.classList.add('icon')
+            edit_img.src = "./images/edit-button.svg"
+
+            categoryName.innerHTML = product()[0][0];
+
+            categoryActions.appendChild(delete_img);
+            categoryActions.appendChild(edit_img);
+            category.appendChild(categoryName);
+            category.appendChild(categoryActions);
+
+            document.getElementById("products-table").appendChild(category);
             document.getElementById("add-product-name").value = "";
             document.getElementById("add-product-price").value = "";
         })
