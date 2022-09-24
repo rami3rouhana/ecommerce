9f6cd7e91aafd1ce224977614cfd39bdc11a54ac
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-24 14:57:00 +0300

    frontend: add category

--- seller-frontend/scripts/AddCategory.js
+++ seller-frontend/scripts/AddCategory.js
@@ -12,10 +12,38 @@ export const AddCategory = (category,setCategory) =>{
             // add category api
     
             // Insert Value into table
-            const categoryName = document.createElement('li');
-            categoryName.classList.add('td');
-            categoryName.innerText = category();
-            document.getElementById("name").appendChild(categoryName);
+            const categoryRow = document.createElement('tr');
+            categoryRow.classList.add('td');
+            categoryRow.classList.add('color-purple');
+            categoryRow.classList.add('border');
+            const categoryName = document.createElement('td');
+            const categoryActions = document.createElement('td');
+
+            const delete_img = document.createElement('img');
+            delete_img.classList.add('icon')
+            delete_img.src = "./images/delete-button.svg"
+
+            const edit_img = document.createElement('img');
+            edit_img.classList.add('icon')
+            edit_img.src = "./images/edit-button.svg"
+
+            categoryName.innerHTML = category()[0][0];
+
+            categoryActions.appendChild(delete_img);
+            categoryActions.appendChild(edit_img);
+
+            delete_img.addEventListener('click', () => {
+                console.log('delete')
+            })
+
+            edit_img.addEventListener('click', () => {
+                console.log('edit')
+            })
+            categoryRow.appendChild(categoryName);
+            categoryRow.appendChild(categoryActions);
+
+            document.getElementById("categories-table").appendChild(categoryRow);
+
             document.getElementById("add-category-name").value = '';
         })
     }
