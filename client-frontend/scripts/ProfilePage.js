8b669b077aa2d8e88454245725d2e04da41acd9a
Mohamad Nassar <mnassar57@gmail.com>
2022-09-25 10:55:09 +0300

    frontend: updating profile page

--- client-frontend/scripts/ProfilePage.js
+++ client-frontend/scripts/ProfilePage.js
@@ -56,7 +56,9 @@ export const ProfilePage =  async () => {
     async function SetProfilePicture() {
         //Change profile card
         const url = "http://localhost/ecommerce/ecommerce-server/receive-userinfo.php";
-        const response = await axios.post(url, {}, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}});
+        const response = await axios.post(url, {}, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}}).then(response => {
+            console.log(response);
+        });
         let userid = (response.data['userid']);
         let user_name = (response.data['name']);
         let user_email = (response.data['email']);
