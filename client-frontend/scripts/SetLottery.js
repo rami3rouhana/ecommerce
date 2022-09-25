2b38c004e0d17c145f9f0e079fa6002c4e9474d9
Mohamad Nassar <mnassar57@gmail.com>
2022-09-25 18:17:20 +0300

    frontend: lottery matching implemented

--- client-frontend/scripts/SetLottery.js
+++ client-frontend/scripts/SetLottery.js
@@ -1,16 +1,29 @@
-export const SetLottery = () => {
+export const SetLottery = async () => {
     if(document.getElementById('lottery-btn')){
         const nums = Array.prototype.slice.call(document.getElementsByClassName('num'));
-        document.getElementById('lottery-btn').addEventListener('click', () => {
+        document.getElementById('lottery-btn').addEventListener('click', async () => {
         
             nums.forEach(num => {
                 num.disabled = true;
-                console.log(num.value)
+
             });
             document.getElementById('lottery-btn').disabled = true;
+
+                    //Get lotteries
+        const url = "http://localhost/ecommerce/ecommerce-server/receive-lotteries.php";
+        const messages = await axios.post(url, {}, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}});
+        let lotteries = (messages.data.lotteries);
+        lotteries.forEach(lottery => {
+            for(let i = 0; i < nums.length; i++){
+                //console.log(nums[i].value);
+                if(nums[i].value == (lottery.random_number.toString()[i])){
+                    console.log("match");
+                }
+            }
+        })
         })
 
-        //Get lotteries
-        console.log(nums);
+
+
     }
 }
