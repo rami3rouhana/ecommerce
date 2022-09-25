10e657e18ecd12f437a57afadab285c95f2a4c26
Mohamad Nassar <mnassar57@gmail.com>
2022-09-25 11:52:00 +0300

    frontend: profile general fixes

--- client-frontend/scripts/ProfilePage.js
+++ client-frontend/scripts/ProfilePage.js
@@ -1,4 +1,15 @@
 export const ProfilePage =  async () => {
+
+    if(document.getElementById("profile-img-header")){
+        const url = "http://localhost/ecommerce/ecommerce-server/receive-userinfo.php";
+        const response = await axios.post(url, {}, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}})
+        console.log(response.data['userid']);
+        let userid = (response.data['userid']);
+        let user_name = (response.data['name']);
+        let user_email = (response.data['email']);
+        document.getElementById("profile-img-header").setAttribute('src', "http://localhost/ecommerce/client-frontend/images/uploadedimages/" + userid + "-" + user_name + ".jpg");
+    }
+
     if(document.getElementById("update-popup-btn")){
         document.getElementById("update-popup-btn").addEventListener("click", () => {
             console.log("3aw");
@@ -62,17 +73,24 @@ export const ProfilePage =  async () => {
             let user_name = (response.data['name']);
             let user_email = (response.data['email']);
             //document.getElementById("profile-picture-card").setAttribute('src', "http://localhost/ecommerce/client-frontend/images/uploadedimages/" + userid + "-" + user_name + ".jpg");
+            
+            if(document.getElementById("profile-picture-card"))
             document.getElementById("profile-picture-card").src = ("http://localhost/ecommerce/client-frontend/images/uploadedimages/" + userid + "-" + user_name + ".jpg");
-    
-            //Change profile header
-            document.getElementById("profile-img-header").setAttribute('src', "http://localhost/ecommerce/client-frontend/images/uploadedimages/" + userid + "-" + user_name + ".jpg");
-    
+            
             //Set profile name
+            if(document.getElementById("profile-card-name"))
             document.getElementById("profile-card-name").innerHTML = user_name;
             
             //Set profile email
+            if(document.getElementById("profile-card-email"))
             document.getElementById("profile-card-email").innerHTML = user_email;
         });
 
     }
+
+    async function SetProfileHeader(){
+        //Change profile header
+        await SetProfilePicture();
+    }
+    SetProfileHeader();
 }
