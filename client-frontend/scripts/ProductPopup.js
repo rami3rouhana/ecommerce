29e7cb00145fd2c92ac5b61d74b2852e9ba978c1
Mohamad Nassar <mnassar57@gmail.com>
2022-09-25 10:55:27 +0300

    frontend: product popup fixed

---
+++ client-frontend/scripts/ProductPopup.js
@@ -0,0 +1,23 @@
+export const ProductPopup = async (product_id) => {
+    document.getElementById("product-popup-form").classList.remove('hidden');
+    let productTitle = document.getElementById("product-popup-title");
+    let productPrice = document.getElementById("product-popup-price");
+    let productDescription = document.getElementById("product-popup-description");
+    let productPicture = document.getElementById("product-popup-picture");
+
+    //Get product from database
+    const url = "http://localhost/ecommerce/ecommerce-server/receive-product.php";
+    const response = await axios.post(url, {product_id: product_id}, { headers: {'Authorization': `token ${localStorage.getItem(`token`)}`}});
+    let dataResponse = (response.data['product_info'][0]);
+
+    //Update popup
+    productTitle.innerHTML = (dataResponse.name);
+    productPrice.innerHTML = (dataResponse.price) + ("$");
+    productPicture.setAttribute('src', dataResponse.picture_url);
+    productDescription.innerHTML = ("Seller Name: ") + (dataResponse.seller_name);
+    //productPicture.innerHTML = (dataResponse.name);
+
+
+    //Increment views on picture
+    
+}
