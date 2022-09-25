7d6b0c4e670774ff92a4275e5ab87dc69d9a9c5f
Mohamad Nassar <mnassar57@gmail.com>
2022-09-25 17:39:24 +0300

    frontend: send voucher implemented

--- client-frontend/scripts/SendVoucher.js
+++ client-frontend/scripts/SendVoucher.js
@@ -1,14 +1,23 @@
-export const SendVoucher = () => {
+export const SendVoucher = async () => {
     if(document.getElementById("voucher-email")){
 
         //Find voucher value
-        document.getElementById('send-voucher').addEventListener("click", () => {
+        document.getElementById('send-voucher').addEventListener("click", async () => {
             let ele = document.getElementsByName('prices');
             //console.log("gg");
             //console.log(ele);
             for(let i = 0; i < ele.length; i++) {
                 if(ele[i].checked){
-                    console.log(ele[i].value)
+                    let voucherValue = (ele[i].value);
+                    let voucherCode = document.getElementById("voucher-code").value;
+                    let emailToSendTo = document.getElementById("voucher-email").value;
+                    console.log(voucherValue, emailToSendTo, voucherCode);
+                    const url = "http://localhost/ecommerce/ecommerce-server/add-vouchers.php";
+                    let data = {"code": voucherCode,
+                                "email": emailToSendTo,
+                                "amount": voucherValue}
+                    const response = await axios.post(url, data, {headers: {'Authorization': `token ${localStorage.getItem("token")}` }});
+                    console.log(response);
                 }
             }
         })
