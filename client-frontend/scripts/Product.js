baae75fc3d2a0e6f26c77a7c0b2455aeb62a343e
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-26 14:22:11 +0300

    frontend: add styling products

--- client-frontend/scripts/Product.js
+++ client-frontend/scripts/Product.js
@@ -6,21 +6,21 @@ export const Product = (product) => {
     </div>
 
     <div class="info">
-      <div class="info-details">
-        <div class="product-name">
-            <span class="color-purple">${product.name}</span>
+        <div class="info-details">
+          <div class="product-name">
+              <span class="color-purple">${product.name}</span>
+          </div>
+          <div class="product-price">
+              <span class="color-purple">${product.price}$</span>
+          </div>
+          <div class="icons">
+            <span id="${product.id}" class="pointer color-purple"><i class="fa fa-star"></i></span>
+            <span id="${product.id}" class="pointer color-purple"><i class="fa fa-list" aria-hidden="true"></i></span>
+            <span id="${product.id}" class="pointer color-purple"
+              ><i class="fa fa-shopping-cart" aria-hidden="true"></i
+            ></span>
+          </div>
         </div>
-        <div class="product-price">
-            <span class="color-purple">${product.price}$</span>
-        </div>
-      </div>
-      <div class="icons">
-        <span id="${product.id}" class="pointer color-purple"><i class="fa fa-star"></i></span>
-        <span id="${product.id}" class="pointer color-purple"><i class="fa fa-list" aria-hidden="true"></i></span>
-        <span id="${product.id}" class="pointer color-purple"
-          ><i class="fa fa-shopping-cart" aria-hidden="true"></i
-        ></span>
-      </div>
     </div>
   </div>
     `)
