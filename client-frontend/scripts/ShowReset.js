7d6ff55257b373c2e5e210b629ba75dfe69988e4
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-26 15:14:37 +0300

    frontend: update reset password to popup

--- client-frontend/scripts/ShowReset.js
+++ client-frontend/scripts/ShowReset.js
@@ -4,17 +4,11 @@ export const ShowReset = () =>{
     if(document.getElementById("reset-password"))
     document.getElementById("reset-password").addEventListener("click", (e)=>{
         e.preventDefault();
-        if(document.getElementById("email-reset").classList[0] === "hidden-reset"){
-        document.getElementById("email-reset").classList.remove("hidden-reset");
-        document.getElementById("email-reset").classList.add("reset");
-        document.getElementById("login-btn").innerText = "Reset";
-        document.getElementById("login-btn").id = "reset-btn";
+        document.getElementById('popup-reset-password').classList.remove('hidden');
         ResetPassword();
-        }else{
-            document.getElementById("email-reset").classList.remove("reset");
-            document.getElementById("email-reset").classList.add("hidden-reset");
-            document.getElementById("reset-btn").innerText = "Login";
-            document.getElementById("reset-btn").id = "login-btn";
-        }
     });
+
+    document.getElementById('close-btn').addEventListener("click", ()=> {
+        document.getElementById('popup-reset-password').classList.add('hidden');
+    })
 }
