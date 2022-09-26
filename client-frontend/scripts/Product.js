a3600385c4e019320e1dc22536fa1c0d9f4f0f71
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-26 14:06:33 +0300

    frontend: add styling to product page and popup

--- client-frontend/scripts/Product.js
+++ client-frontend/scripts/Product.js
@@ -1,23 +1,27 @@
 export const Product = (product) => {
     return (`
-        <div class="column" id="${product.id}">
-        <div class="image-product">
-          <img src="${product.picture_url}" alt="" />
-        </div>
+    <div class="column product-col" id="${product.id}">
+    <div class="image-product">
+      <img src="${product.picture_url}" alt="" />
+    </div>
 
-        <div class="info">
-          <div class="info-details">
+    <div class="info">
+      <div class="info-details">
+        <div class="product-name">
             <span class="color-purple">${product.name}</span>
+        </div>
+        <div class="product-price">
             <span class="color-purple">${product.price}$</span>
-          </div>
-          <div class="icons">
-            <span id="${product.id}" class="pointer color-purple"><i class="fa fa-star"></i></span>
-            <span id="${product.id}" class="pointer color-purple"><i class="fa fa-list" aria-hidden="true"></i></span>
-            <span id="${product.id}" class="pointer color-purple"
-              ><i class="fa fa-shopping-cart" aria-hidden="true"></i
-            ></span>
-          </div>
         </div>
       </div>
+      <div class="icons">
+        <span id="${product.id}" class="pointer color-purple"><i class="fa fa-star"></i></span>
+        <span id="${product.id}" class="pointer color-purple"><i class="fa fa-list" aria-hidden="true"></i></span>
+        <span id="${product.id}" class="pointer color-purple"
+          ><i class="fa fa-shopping-cart" aria-hidden="true"></i
+        ></span>
+      </div>
+    </div>
+  </div>
     `)
 }
